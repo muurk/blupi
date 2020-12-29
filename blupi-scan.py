bf73b4d05955da99082f01a5d11af30393dcaac2
mr <mark@nugatory.co.uk>
2020-12-29 23:57:58 +0000

    * Introduced argparse * Shifted most global variables into argparse parameters * Previously defined global variables are now set as argparse defaults * Introduced logging module * Switched out all print "" statement with calls to the logger function

--- blupi-scan.py
+++ blupi-scan.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import sys
+__doc__ = "blupi"
+
+import argparse
+import logging
 import time
 import os
 import subprocess as sp
@@ -8,17 +11,41 @@ from os import devnull
 from collections import deque
 from math import sqrt
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+required_params = parser.add_argument_group("Required Arguments")
+optional_params = parser.add_argument_group("Optional Arguments")
+optional_params.add_argument('--freqmin', help="Minimum Frequency", type=int, default=854000000)
+optional_params.add_argument('--freqmax', help="Maximum Frequency", type=int, default=860000000)
+optional_params.add_argument('--sensitivity', help="Sensitivity", type=int, default=50)
+optional_params.add_argument('--sysdamping', help="Sysdamping", type=int, default=10)
+optional_params.add_argument('--freqdamping', help="Freqdamping", type=int, default=100)
+optional_params.add_argument("--powerfftw_path", help="Path to rtl_power_fftw binary", type=str,
+							 default="/usr/local/bin/rtl_power_fftw")
+optional_params.add_argument("--baseline_path", help="Baseline Data", type=str,
+							 default="baseline_data.dat")
+optional_params.add_argument("--ppm_offset", help="PPM Offset", type=int, default=56)
+optional_params.add_argument("--bline_int_t", help="bline_int_t", type=int, default=600)
+optional_params.add_argument("--debug", help="Verbose Messaging", action='store_true', default=False)
+
+args = parser.parse_args()
+
+if args.debug is True:
+	logger.setLevel(logging.DEBUG)
+
 # Global variables until we have a config file
-freqmin = 854000000 # DO NOT CHANGE
-freqmax = 860000000 # DO NOT CHANGE
-sensitivity = 50
-sysdamping = 10
-freqdamping = 100
-powerfftw_path = "/usr/local/bin/rtl_power_fftw"
-baseline_path = "baseline_data.dat"
+freqmin = args.freqmin
+freqmax = args.freqmax
+sensitivity = args.sensitivity
+sysdamping = args.sysdamping
+freqdamping = args.freqdamping
+powerfftw_path = args.powerfftw_path
+baseline_path = args.baseline_path
 totalbins = 960 * 3 # DO NOT CHANGE
-ppm_offset = 56
-bline_int_t = 600 # Set to around 600 (10m)
+ppm_offset = args.ppm_offset
+bline_int_t = args.bline_int_t
 
 # Global that we'll keep
 dvnll = open(devnull, 'wb')
@@ -34,10 +61,9 @@ def alert(p):
 
 	# Still developing...need to provide true alert functionality (GPIO, audio, etc)
 	freq = round(p[0] /1000000, 4)
-	print "At " + time.strftime("%H:%M:%S") + ", a " + str(round(p[1], 1)) + " dB/Hz signal was detected at " + str(freq) + " MHz."
+	logger.debug("At " + time.strftime("%H:%M:%S") + ", a " + str(round(p[1], 1)) + " dB/Hz signal was detected at " + str(freq) + " MHz.")
 
 def bline_build(fmin, fmax, bins, b_time, offset, bpath):
-	
 	t = "-t " + str(b_time)
 	array = []
 	spect = "-f " + str(fmin) + ":" + str(fmax)
@@ -51,7 +77,6 @@ def bline_build(fmin, fmax, bins, b_time, offset, bpath):
 
 # Start your engines...
 if __name__ == '__main__':
-
 	# Import settings file here...
 
 	# Parameter formatting
@@ -68,20 +93,17 @@ if __name__ == '__main__':
 	i = 0
 	stddev = 100
 	baseline_file = "-B " + str(baseline_path)
-	
 
 	# Ready, set, GO!
 	try:
-		print "Starting up at", time.strftime("%H:%M:%S") + "..."
+		logger.debug("Starting up")
 		
 		# Check for baseline file
 		if not os.path.isfile(baseline_path):
-
-			print "I couldn't find a baseline file, so we're gonna generate one. This will take some time..."
-
+			logger.debug(f"{baseline_path}: Baseline file not found. Generating a new one.")
 			# Generate said baseline file
 			bline_build(freqmin, freqmax, fftb, bline_int_t, ppm, baseline_path)
-			print "Baseline file generation completed at", time.strftime("%H:%M:%S") + ". Starting up!"
+			logger.debug(f"{baseline_path}: Baseline file generation completed.")
 
 		rpf = sp.Popen([powerfftw_path, spect, otherargs, ppm, fftbins, baseline_file], stdout=sp.PIPE, stderr=dvnll, shell=False)
 
@@ -126,8 +148,7 @@ if __name__ == '__main__':
 						rolling_avg.popleft()
 						stddev = std_dev(rolling_avg)
 
-
 	except (KeyboardInterrupt, SystemExit): # Press ctrl-c
 
 		rpf.kill()
-		print "\n", "Buh-bye"
+		logger.debug("\n", "Buh-bye")
